b19fae083529aac684a41fb20635e3f3ad84f602
Nate Coraor <nate@bx.psu.edu>
2018-03-29 13:56:09 -0400

    Travis config for pushing libpython builds to hub/quay

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-tag="quay.io/pypa/manylinux1_$PLATFORM"
+tag="quay.io/natefoo/manylinux1_$PLATFORM"
 docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
+docker push ${tag}:latest
+docker login -u $HUB_USERNAME -p $HUB_PASSWORD hub.docker.com
+tag="natefoo/manylinux1_$PLATFORM"
 docker push ${tag}:latest
